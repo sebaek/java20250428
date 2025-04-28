a9c88387a21ef51f74086c906ef6eaa9c3e81367
sebaek <sebaek@gmail.com>
2025-04-28 16:30:08 +0900

    String Block

---
+++ src/ch02/sec06/TextBlockExample.java
@@ -0,0 +1,20 @@
+package ch02.sec06;
+
+public class TextBlockExample {
+    public static void main(String[] args) {
+        String str1 = "" +
+                "{\n" +
+                "\t\"id\":\"winter\",\n" +
+                "\t\"name\":\"눈송이\"\n" +
+                "}";
+
+        System.out.println("str1 = " + str1);
+
+        String str2 = """
+                {
+                    "id":"winter",
+                    "name":"눈송이"
+                }""";
+        System.out.println("str2 = " + str2);
+    }
+}
