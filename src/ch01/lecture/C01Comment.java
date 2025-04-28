a99ae9a855a08134d68cc43b0c1dfd062b157319
sebaek <sebaek@gmail.com>
2025-04-28 12:12:18 +0900

    first commit

---
+++ src/ch01/lecture/C01Comment.java
@@ -0,0 +1,18 @@
+package ch01.lecture;
+
+public class C01Comment {
+    public static void main(String[] args) {
+        // 한 줄 주석
+        /*
+        여러 줄 주석
+        여러 줄 주석
+         */
+//         ctrl /
+        // 주석: 프로그램실행에
+        //     영향을 미치지 않음
+
+//        System.out.println("hi");
+        System.out.println("java");
+//        System.out.println("자바");
+    }
+}
---
+++ src/ch02/lecture/C03Variable.java
@@ -0,0 +1,23 @@
+package ch02.lecture;
+
+public class C03Variable {
+    public static void main(String[] args) {
+        // javascript 코드
+//        let a = 3;
+//        a = "java";
+//        a = 3.14;
+
+        int b = 3;
+//        b = "java";
+//        b = 3.14;
+
+        // 변수의 타입에 맞는 값만 변수에 넣을 수 있음
+        int c = 9; // 정수
+        double d = 3.14; // 실수
+        String e = "java"; // 문자열(text)
+
+//        c = 9.12; // x
+//        e = 3; // x
+//        d = "hi"; // x
+    }
+}
