fc679199adf2b67b2ba5243b3f11b5c8c1821b40
sebaek <sebaek@gmail.com>
2025-04-28 16:43:58 +0900

    변수명 규칙,관습

---
+++ src/ch02/lecture/C12VariableName.java
@@ -0,0 +1,19 @@
+package ch02.lecture;
+
+public class C12VariableName {
+    public static void main(String[] args) {
+        // 영문대소문자,$,_,숫자
+        // 숫자로 시작할 수 없음
+        // 의미있는 이름으로
+        // lowerCamelCase 사용
+        // 예약어(키워드)사용 불가
+
+        int modelName; // ok (관습에 적합)
+//        int class; // class가 예약어임
+//        int 6hour; // 숫자로 시작할 수 없음
+        int $value; // ok (관습에 적합하지 않음)
+        int _age; // ok (관습에 적합하지 않음)
+//        int #name; // #은 사용불가
+//        int int; // int가 예약어임
+    }
+}
