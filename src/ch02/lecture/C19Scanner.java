774777d29dca9ec512405e4e399269c5ac2b115b
sebaek <sebaek@gmail.com>
2025-04-29 11:54:01 +0900

    스캐너

---
+++ src/ch02/lecture/C19Scanner.java
@@ -0,0 +1,27 @@
+package ch02.lecture;
+
+
+import java.util.Scanner;
+
+public class C19Scanner {
+    public static void main(String[] args) {
+        System.out.println("프로그램 시작");
+
+        // 커서를 이동하면서 입력된 값을 읽어냄
+        Scanner scanner = new Scanner(System.in);
+
+        // 엔터를 만날 때까지 읽은 내용을 리턴함
+        String s = scanner.nextLine();
+
+        System.out.println("s = " + s);
+
+        String t = scanner.nextLine();
+
+        System.out.println("t = " + t);
+
+
+        System.out.println("프로그램 종료");
+
+
+    }
+}
