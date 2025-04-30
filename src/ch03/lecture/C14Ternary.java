727fd80673751db7c417424988ba0a5565277840
sebaek <sebaek@gmail.com>
2025-04-30 09:49:29 +0900

    삼항연산자

---
+++ src/ch03/lecture/C14Ternary.java
@@ -0,0 +1,16 @@
+package ch03.lecture;
+
+public class C14Ternary {
+    public static void main(String[] args) {
+        //삼항연산자(Ternary)
+        //조건연산자(Conditional)
+
+        // 항1 ? 항2 : 항3
+
+        int r1 = true ? 3 : 5; // 3
+        int r2 = false ? 9 : 11; // 11
+        System.out.println("r1 = " + r1);
+        System.out.println("r2 = " + r2);
+        
+    }
+}
