50dabfb2a5e8aaf710f31dd38b1c3af357b88980
sebaek <sebaek@gmail.com>
2025-04-30 11:30:28 +0900

    random

--- src/ch04/practice/C01Random.java
+++ src/ch04/practice/C01Random.java
@@ -7,6 +7,16 @@ public class C01Random {
         // 같은 값이 나오면 "당첨" 출력
         //          아니면 "다시 돌려보세요" 출력
 
-        
+        int a = ((int) (Math.random() * 6)) + 1;
+        int b = ((int) (Math.random() * 6)) + 1;
+
+        System.out.println("주사위1 = " + a);
+        System.out.println("주사위2 = " + b);
+        if (a == b) {
+            System.out.println("당첨");
+        } else {
+            System.out.println("다시 돌려보세요.");
+        }
+
     }
 }
