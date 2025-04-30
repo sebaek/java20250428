2f2e3b6478b43d3783aae853628547d19955e9bd
sebaek <sebaek@gmail.com>
2025-04-30 11:21:20 +0900

    random

---
+++ src/ch04/practice/C01Random.java
@@ -0,0 +1,12 @@
+package ch04.practice;
+
+public class C01Random {
+    public static void main(String[] args) {
+
+        // 두 개의 주사위를 돌려서
+        // 같은 값이 나오면 "당첨" 출력
+        //          아니면 "다시 돌려보세요" 출력
+
+        
+    }
+}
