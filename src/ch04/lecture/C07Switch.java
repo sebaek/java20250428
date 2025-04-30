0d83b6414a7215dd5d16a36d418f629f6745a90e
sebaek <sebaek@gmail.com>
2025-04-30 12:10:32 +0900

    switch

---
+++ src/ch04/lecture/C07Switch.java
@@ -0,0 +1,15 @@
+package ch04.lecture;
+
+public class C07Switch {
+    public static void main(String[] args) {
+//        long a = 3L;
+//        switch (a) {
+//            case 1L:
+//                System.out.println("1");
+//            case 2L:
+//                System.out.println("2");
+//            case 3L:
+//                System.out.println("3");
+//        }
+    }
+}
