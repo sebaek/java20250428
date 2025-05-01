4d43ba0867f3c5ed21eab4649ebb6468ca33b425
sebaek <sebaek@gmail.com>
2025-05-01 09:41:26 +0900

    for문 연습

--- src/ch04/practice/C02For.java
+++ src/ch04/practice/C02For.java
@@ -24,11 +24,52 @@ public class C02For {
             System.out.println(i);
         }
 
+        System.out.println("---------------");
         // 0 2 4 6 8
+        for (int i = 0; i < 9; i++) {
+            if ((i % 2) == 0) {
+                System.out.println(i);
+            }
+        }
+        System.out.println("---------------");
+        // 0 2 4 6 8
+        for (int i = 0; i < 9; i += 2) {
+            System.out.println(i);
+        }
+
+        System.out.println("---------------");
         // 2 4 6 8 10
+        for (int i = 2; i <= 10; i++) {
+            if ((i % 2) == 0) {
+                System.out.println(i);
+            }
+        }
+        System.out.println("---------------");
+        // 2 4 6 8 10
+        for (int i = 2; i <= 10; i += 2) {
+            System.out.println(i);
+        }
+
+        System.out.println("---------------");
         // 4 3 2 1 0
+        for (int i = 4; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("---------------");
         // 5 4 3 2 1
+        for (int i = 5; i > 0; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("---------------");
         // * * * * *
+        for (int i = 0; i < 5; i++) {
+            System.out.println("*");
+        }
+
+
+        System.out.println("---------------");
         /*
          *****
          *****
@@ -36,5 +77,8 @@ public class C02For {
          *****
          *****
          */
+        for (int i = 0; i < 5; i++) {
+            System.out.println("*****");
+        }
     }
 }
