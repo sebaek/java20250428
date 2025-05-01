210e46023787729c39d3b1ec7811ca910216d40b
sebaek <sebaek@gmail.com>
2025-05-01 12:32:47 +0900

    중첩된 for문 연습

---
+++ src/ch04/lecture/C20For.java
@@ -0,0 +1,16 @@
+package ch04.lecture;
+
+public class C20For {
+    public static void main(String[] args) {
+        // debugging
+
+        System.out.println("코드1");
+        System.out.println("코드2");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+
+    }
+}
