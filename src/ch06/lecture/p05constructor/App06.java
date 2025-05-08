f6f9fbfc55bffe6438f09b46c3463540ce2e4a89
sebaek <sebaek@gmail.com>
2025-05-08 16:18:04 +0900

    파라미터 없는 생성자 자동 추가되거나 되지 않음

---
+++ src/ch06/lecture/p05constructor/App06.java
@@ -0,0 +1,57 @@
+package ch06.lecture.p05constructor;
+
+public class App06 {
+    public static void main(String[] args) {
+        MyClass61 a = new MyClass61();
+        MyClass61 b = new MyClass61("a");
+        MyClass61 c = new MyClass61(3);
+
+        MyClass62 d = new MyClass62();
+
+//        MyClass63 e = new MyClass63();
+        MyClass63 f = new MyClass63(3);
+
+        MyClass64 g = new MyClass64();
+    }
+}
+
+class MyClass64 {
+    // 파라미터 있는 생성자와
+    // 파라미터 없는 생성자가 모두 필요하면
+    // 다 작성해야 함
+    MyClass64() {
+
+    }
+
+    MyClass64(int a) {
+
+    }
+}
+
+class MyClass63 {
+    // 파라미터 있는 생성자를 만들면
+    // 파라미터 없는 생성자는 자동으로 추가되지 않음
+    MyClass63(int a) {
+
+    }
+}
+
+class MyClass62 {
+    // 생성자를 작성하지 않으면 파라미터 없는 생성자가 자동으로 추가됨
+//    public MyClass62() {
+//    }
+}
+
+class MyClass61 {
+    MyClass61() {
+
+    }
+
+    MyClass61(String a) {
+
+    }
+
+    MyClass61(int b) {
+
+    }
+}
