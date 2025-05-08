47fd7357404f952b9e79ddb3a54bffb223d053ee
sebaek <sebaek@gmail.com>
2025-05-08 09:53:22 +0900

    field

---
+++ src/ch06/sec06/exam01/Car.java
@@ -0,0 +1,10 @@
+package ch06.sec06.exam01;
+
+public class Car {
+
+    // 필드에 초기값은
+    // 0, false, null
+    String model;
+    boolean start;
+    int speed;
+}
---
+++ src/ch06/sec06/exam01/CarExample.java
@@ -0,0 +1,14 @@
+package ch06.sec06.exam01;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+//        c1.model = "tesla";
+//        c1.start = true;
+//        c1.speed = 100;
+
+        System.out.println("c1.model = " + c1.model);
+        System.out.println("c1.start = " + c1.start);
+        System.out.println("c1.speed = " + c1.speed);
+    }
+}
