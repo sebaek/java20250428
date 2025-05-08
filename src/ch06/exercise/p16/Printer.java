9e3c9636cf4386c04f8bc4bf1003f32d31b5c3b8
sebaek <sebaek@gmail.com>
2025-05-08 14:15:39 +0900

    method overloading 확인문제 16

---
+++ src/ch06/exercise/p16/Printer.java
@@ -0,0 +1,22 @@
+package ch06.exercise.p16;
+
+public class Printer {
+    void println(int p) {
+        System.out.println(p);
+    }
+
+    void println(double p) {
+        System.out.println(p);
+
+    }
+
+    void println(String p) {
+        System.out.println(p);
+
+    }
+
+    void println(boolean p) {
+        System.out.println(p);
+
+    }
+}
