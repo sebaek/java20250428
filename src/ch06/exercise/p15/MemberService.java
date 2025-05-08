fcff484b917c6533a26a142d72e9df5d8c9238fa
sebaek <sebaek@gmail.com>
2025-05-08 12:16:34 +0900

    확인문제 15

---
+++ src/ch06/exercise/p15/MemberService.java
@@ -0,0 +1,19 @@
+package ch06.exercise.p15;
+
+public class MemberService {
+
+    public boolean login(String id, String password) {
+
+        if (id.equals("hong")) {
+            if (password.equals("12345")) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void logout(String id) {
+        System.out.println(id + "님이 로그아웃되었습니다.");
+    }
+}
