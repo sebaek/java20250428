bb7d9e4e8ec0b2431f973edbbb7299fa1b5956d8
sebaek <sebaek@gmail.com>
2025-05-08 14:44:32 +0900

    member, this

---
+++ src/ch06/sec09/CarExample.java
@@ -0,0 +1,11 @@
+package ch06.sec09;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car c1 = new Car("포르쉐");
+        Car c2 = new Car("벤츠");
+
+        c1.run();
+        c2.run();
+    }
+}
