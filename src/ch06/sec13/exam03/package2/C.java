748b4b47fac8351fc4efc5be52afc335adeb2188
sebaek <sebaek@gmail.com>
2025-05-09 14:31:28 +0900

    접근제한자 access modifier

---
+++ src/ch06/sec13/exam03/package2/C.java
@@ -0,0 +1,16 @@
+package ch06.sec13.exam03.package2;
+
+import ch06.sec13.exam03.package1.A;
+
+public class C {
+    public C() {
+        A a = new A();
+        a.field1 = 1;
+//        a.field2 = 2;
+//        a.field3 = 3;
+
+        a.method1();
+//        a.method2();
+//        a.method3();
+    }
+}
