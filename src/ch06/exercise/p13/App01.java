4ea5d7fbc2ee1e73dbf777b56c4479f04339f07d
sebaek <sebaek@gmail.com>
2025-05-09 15:27:12 +0900

    encapsulation, getter, setter

--- src/ch06/exercise/p13/App01.java
+++ src/ch06/exercise/p13/App01.java
@@ -3,12 +3,16 @@ package ch06.exercise.p13;
 public class App01 {
     public static void main(String[] args) {
         Member m1 = new Member();
-        m1.name = "도날드";
-        m1.age = 99;
+//        m1.name = "도날드";
+//        m1.age = 99;
+        m1.setName("도날드");
+        m1.setAge(99);
 
         Member m2 = new Member();
-        m2.name = "트럼프";
-        m2.age = 88;
-        
+//        m2.name = "트럼프";
+//        m2.age = 88;
+        m2.setName("트럼프");
+        m2.setAge(88);
+
     }
 }
