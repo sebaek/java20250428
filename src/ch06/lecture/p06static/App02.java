c7632e26eed1014e0c6e397870a1e571a58ede3f
sebaek <sebaek@gmail.com>
2025-05-09 09:35:35 +0900

    static block

---
+++ src/ch06/lecture/p06static/App02.java
@@ -0,0 +1,37 @@
+package ch06.lecture.p06static;
+
+public class App02 {
+    public static void main(String[] args) {
+        System.out.println(MyClass02.number);
+        MyClass02 a = new MyClass02();
+        System.out.println("a.age = " + a.age);
+    }
+}
+
+class MyClass02 {
+    // static field
+    static int number;
+
+    // static block
+    static {
+        // static filed 초기화 코드 작성 가능
+        int a = 3;
+        if (true) {
+            a *= 10;
+        }
+        number = a;
+    }
+
+    // instance field
+    int age;
+
+    MyClass02() {
+        int a = 3;
+        if (true) {
+            a = a * 10;
+        }
+        this.age = a;
+    }
+
+
+}
