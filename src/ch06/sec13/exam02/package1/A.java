b86181a01662b76556ca2c46d39d5f941359aa2c
sebaek <sebaek@gmail.com>
2025-05-09 14:37:14 +0900

    접근제한자 access modifier

---
+++ src/ch06/sec13/exam02/package1/A.java
@@ -0,0 +1,16 @@
+package ch06.sec13.exam02.package1;
+
+public class A {
+    A a1 = new A(true);
+    A a2 = new A(3);
+    A a3 = new A("s");
+
+    public A(boolean b) {
+    }
+
+    A(int b) {
+    }
+
+    private A(String s) {
+    }
+}
