558824aec19bb820bfc718a1b94afc4d75b70668
sebaek <sebaek@gmail.com>
2025-05-09 14:40:39 +0900

    접근제한자 access modifier

---
+++ src/ch06/lecture/p09access/App05.java
@@ -0,0 +1,7 @@
+package ch06.lecture.p09access;
+
+public class App05 {
+    public static void main(String[] args) {
+        MyClass05 a = new MyClass05();
+    }
+}
---
+++ src/ch06/lecture/p09access/sub1/App05.java
@@ -0,0 +1,9 @@
+package ch06.lecture.p09access.sub1;
+
+import ch06.lecture.p09access.MyClass05;
+
+public class App05 {
+    public static void main(String[] args) {
+        MyClass05 a = new MyClass05();
+    }
+}
