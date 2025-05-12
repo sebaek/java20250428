37f690ed0a1076226b91078f0bf074227035098e
sebaek <sebaek@gmail.com>
2025-05-12 14:36:33 +0900

    변수의 타입이 가진 메소드만 실행가능하다

---
+++ src/ch07/ch07/sec07/exam02/Child.java
@@ -0,0 +1,15 @@
+package ch07.ch07.sec07.exam02;
+
+public class Child extends Parent {
+    // method1
+
+
+    @Override
+    public void method2() {
+        System.out.println("Child.method2");
+    }
+
+    public void method3() {
+        System.out.println("Child.method3");
+    }
+}
