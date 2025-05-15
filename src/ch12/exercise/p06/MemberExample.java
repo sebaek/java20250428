8c33a33629c93ff91ee0bea43bd1b88ff78b8d26
sebaek <sebaek@gmail.com>
2025-05-15 14:09:16 +0900

    toString

---
+++ src/ch12/exercise/p06/MemberExample.java
@@ -0,0 +1,10 @@
+package ch12.exercise.p06;
+
+public class MemberExample {
+    public static void main(String[] args) {
+        Member member = new Member("blue", "이파란");
+        Member member1 = new Member("red", "trump");
+        System.out.println(member);
+        System.out.println(member1);
+    }
+}
