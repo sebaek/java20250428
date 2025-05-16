b1450567a2c689038be21ff1d9e842ab273ca0f4
sebaek <sebaek@gmail.com>
2025-05-16 11:35:44 +0900

    정규표현식 예제

---
+++ src/ch12/lecture/p04regex/App10.java
@@ -0,0 +1,25 @@
+package ch12.lecture.p04regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class App10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String regex = "010(-| )?\\d{4}(-| )?\\d{4}";
+
+        while (true) {
+            System.out.print("전화번호>");
+            String input = scanner.nextLine();
+
+            boolean result = Pattern.matches(regex, input);
+
+            if (result) {
+                System.out.println("🙆 유효한 전화번호");
+            } else {
+                System.out.println("🙅‍♂️ 유효하지 않는 전화번호");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/ch12/practice/App03.java
@@ -0,0 +1,20 @@
+package ch12.practice;
+
+import java.util.Scanner;
+
+public class App03 {
+    public static void main(String[] args) {
+        // 사용자 입력 받아서
+
+        // 유효한 전화번호 인지 확인
+
+        // 아래 전화번호들은 패턴에 일치하는 것들
+        // 01099998888
+        // 010-9999-8888
+        // 010 9999 8888
+        // 010-99998888
+        // 010 99998888
+
+
+    }
+}
