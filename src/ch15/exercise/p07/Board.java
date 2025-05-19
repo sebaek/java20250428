00f27937eb4a76a3d5356d6e5b76aebacd7b2288
sebaek <sebaek@gmail.com>
2025-05-19 10:19:51 +0900

    확인문제 7

---
+++ src/ch15/exercise/p07/Board.java
@@ -0,0 +1,32 @@
+package ch15.exercise.p07;
+
+public class Board {
+    private String title;
+    private String content;
+
+    public Board(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return title + '-' + content;
+    }
+}
