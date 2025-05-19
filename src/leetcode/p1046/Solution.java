46c80269ebabccdc515be9a5d7315d499ebf7da9
sebaek <sebaek@gmail.com>
2025-05-19 14:50:21 +0900

    leetcode 1046

--- src/leetcode/p1046/Solution.java
+++ src/leetcode/p1046/Solution.java
@@ -1,7 +1,7 @@
 package leetcode.p1046;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class Solution {
@@ -12,11 +12,25 @@ class Solution {
         }
 
         // 1. 정렬
-        // 2. 마지막index의 요소와, 마지막-1 index의 요소를 꺼내고(지우고)
-        // 3. 차이를 구해서 list에 다시 넣고(이 때 차이가 0이면 넣지 않음)
+        while (list.size() > 1) {
+            Collections.sort(list);
+
+            // 2. 마지막index의 요소와, 마지막-1 index의 요소를 꺼내고(지우고)
+            Integer s1 = list.removeLast();
+            Integer s2 = list.removeLast();
+            // 3. 차이를 구해서 list에 다시 넣고(이 때 차이가 0이면 넣지 않음)
+            int diff = s1 - s2;
+            if (diff != 0) {
+                list.add(diff);
+            }
+        }
         // 1번부터 다시 시작
         // 하나만 남을 때까지 반복해서, 하나남은 값을 리턴
 
-        return list.get(0);
+        if (list.isEmpty()) {
+            return 0;
+        }
+        
+        return list.getFirst();
     }
 }
