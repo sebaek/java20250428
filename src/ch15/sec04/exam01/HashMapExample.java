64e293b1998c10af3d3599e8f945f242f96a2d29
sebaek <sebaek@gmail.com>
2025-05-20 14:16:08 +0900

    전체탐색

--- src/ch15/sec04/exam01/HashMapExample.java
+++ src/ch15/sec04/exam01/HashMapExample.java
@@ -1,7 +1,9 @@
 package ch15.sec04.exam01;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class HashMapExample {
     public static void main(String[] args) {
@@ -15,7 +17,30 @@ public class HashMapExample {
         System.out.println("map.size() = " + map.size());
 
         String key = "홍길동";
-        Integer v = map.get(key);
-        System.out.println("v = " + v);
+        Integer value = map.get(key);
+        System.out.println("value = " + value);
+
+        // 전체 탐색
+        System.out.println();
+        Set<String> keys = map.keySet();
+        Iterator<String> iterator = keys.iterator();
+        while (iterator.hasNext()) {
+            String k = iterator.next();
+            Integer v = map.get(k);
+            System.out.println(k + ":" + v);
+        }
+
+        Set<Map.Entry<String, Integer>> entries = map.entrySet();
+        Iterator<Map.Entry<String, Integer>> iterator1 = entries.iterator();
+        while (iterator1.hasNext()) {
+            Map.Entry<String, Integer> entry = iterator1.next();
+            String k = entry.getKey();
+            Integer v = entry.getValue();
+            System.out.println(k + ":" + v);
+        }
+        System.out.println();
+
+        map.remove("홍길동");
+        System.out.println(map.size());
     }
 }
