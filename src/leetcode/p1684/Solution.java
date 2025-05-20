d593a476a203cf8806b7157759942092a0a7aecc
sebaek <sebaek@gmail.com>
2025-05-20 11:33:16 +0900

    leetcode 1684

---
+++ src/leetcode/p1684/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.p1684;
+
+public class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+        // allowed에 있는 각 문자(Character)를 저장한 Set 만들기
+        // 1. Set 만들고
+        // 2. allowed의 각 문자를 탐색해서
+        // 3. Set에 넣고(add)
+
+        // words에 있는 각 문자열을 전체 탐색해서 각 문자열의 문자가 allowed에 있는 지 확인
+        // 1. words의 각 문자열(word) 탐색
+        //    1. word의 각 문자(character)가
+        //    2. Set에 있는 지 확인(contains))
+        //    3. 모두 다 있으면 count 늘리기
+
+
+        // return count
+        return 0;
+    }
+}
