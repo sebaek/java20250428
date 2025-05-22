699fd0467890c31c9f9b9ee508384b87df918fc4
sebaek <sebaek@gmail.com>
2025-05-22 11:22:37 +0900

    leetcode 2114

---
+++ src/leetcode/p2114/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.p2114;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int mostWordsFound(String[] sentences) {
+        // 고전적 for
+        /*
+        int max = 0;
+        for (String s : sentences) {
+            int length = s.split(" ").length;
+            max = Math.max(max, length);
+        }
+
+        return max;
+
+         */
+
+        // Stream
+        return Arrays.stream(sentences)
+                .map(s -> s.split(" "))
+                .mapToInt(s -> s.length)
+                .max()
+                .getAsInt();
+    }
+}
