2f25dfd585be25bf729562be2ee361a985ce115c
sebaek <sebaek@gmail.com>
2025-05-22 14:34:32 +0900

    leetcode 1748

--- src/leetcode/p1748/Solution.java
+++ src/leetcode/p1748/Solution.java
@@ -1,12 +1,15 @@
 package leetcode.p1748;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class Solution {
     public int sumOfUnique(int[] nums) {
 
-
+        /*
         // nums를 전체 탐색해서 각 아이템이 몇번 나오는지 세기
         Map<Integer, Integer> map = new HashMap<>();
 
@@ -32,5 +35,17 @@ public class Solution {
 
         // 결과 return
         return sum;
+
+         */
+
+
+        return Arrays.stream(nums)
+                .boxed()
+                .collect(groupingBy(identity(), counting())).entrySet()
+                .stream()
+                .filter(e -> e.getValue() == 1)
+                .map(e -> e.getKey())
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 }
