ee06dcf49cf83910f8e4a3a80b4ab000e969698e
sebaek <sebaek@gmail.com>
2025-05-22 14:11:54 +0900

    leetcode 2206

--- src/leetcode/p2206/Solution.java
+++ src/leetcode/p2206/Solution.java
@@ -1,10 +1,18 @@
 package leetcode.p2206;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.groupingBy;
 
 public class Solution {
     public boolean divideArray(int[] nums) {
+        /*
         Map<Integer, Integer> map = new HashMap<>();
 
         // nums에 있는 요소들의 갯수 세기
@@ -24,5 +32,18 @@ public class Solution {
         }
 
         return true;
+
+         */
+
+        // Stream 사용
+
+        return Arrays.stream(nums)
+                .boxed()
+                .collect(groupingBy(identity(), counting()))
+                .values()
+                .stream()
+                .allMatch(e -> e % 2 == 0);
+
+
     }
 }
