f69081ede0517e2164514a5abc0c1270c957f05c
sebaek <sebaek@gmail.com>
2025-05-22 15:18:12 +0900

    Exception

---
+++ src/ch11/lecture/App01.java
@@ -0,0 +1,20 @@
+package ch11.lecture;
+
+public class App01 {
+    public static void main(String[] args) {
+//        String a = null;
+//        int length = a.length(); // NullPointerException
+
+//        int a = 0;
+//        int b = 5;
+//        int c = b / a; // ArithmeticException
+
+//        int[] arr = {3, 4};
+//        System.out.println(arr[2]); // IndexOutOfBoundsException
+
+//        Object d = "java";
+//        Integer e = (Integer) d; // ClassCastException
+
+        System.out.println("프로그램 진행");
+    }
+}
