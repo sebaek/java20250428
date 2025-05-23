ece6ce592d0152cbbfd786b627be1037550aa01d
sebaek <sebaek@gmail.com>
2025-05-23 12:22:15 +0900

    checked exception을 unchecked exception으로 감싸서 다시 던지기

---
+++ src/ch11/lecture/App21.java
@@ -0,0 +1,24 @@
+package ch11.lecture;
+
+public class App21 {
+    public static void main(String[] args) {
+
+        method2();
+    }
+
+    public static void method2() {
+
+        // checked Exception 을 unchecked Exception 감싸서 다시 던지기
+        try {
+            method1();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void method1() throws Exception {
+
+    }
+}
