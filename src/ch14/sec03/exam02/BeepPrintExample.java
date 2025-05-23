fb744abb57f189971f254605a0b3f71b8155f742
sebaek <sebaek@gmail.com>
2025-05-23 14:23:16 +0900

    thread

---
+++ src/ch14/sec03/exam02/BeepPrintExample.java
@@ -0,0 +1,30 @@
+package ch14.sec03.exam02;
+
+import java.awt.*;
+
+public class BeepPrintExample {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            Toolkit toolkit = Toolkit.getDefaultToolkit();
+            for (int i = 0; i < 5; i++) {
+                toolkit.beep();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("띵");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
