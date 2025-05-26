a8e3046f66ed4acde911ef42a1c5b38307e5254a
sebaek <sebaek@gmail.com>
2025-05-26 12:17:25 +0900

    writer

--- src/ch18/lecture/p03writer/App02.java
+++ src/ch18/lecture/p03writer/App02.java
@@ -1,13 +1,30 @@
 package ch18.lecture.p03writer;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 public class App02 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // byte 단위
         // inputStream, outputStream으로
         // 문자(ascii 코드 외)를 읽고 쓰는게 너~~무 불편하다
 
         // 문자(character) 단위
         // reader, writer 가 추가됨
-        
+
+        // writer : 문자 단위 출력 스트림
+
+        String fileName = "C:/Temp/writer02.txt";
+        try (Writer writer = new FileWriter(fileName)) {
+            writer.write('a');
+            writer.write(97);
+            writer.write('걸');
+            writer.write(44152);
+            writer.flush();
+        }
+
+        System.out.println("파일 쓰기 완료");
+
     }
 }
