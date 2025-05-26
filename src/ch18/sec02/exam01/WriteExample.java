cdec7e0668fa8760c143c99a80796dde12483a9b
sebaek <sebaek@gmail.com>
2025-05-26 09:37:52 +0900

    output stream

---
+++ src/ch18/sec02/exam01/WriteExample.java
@@ -0,0 +1,31 @@
+package ch18.sec02.exam01;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class WriteExample {
+    public static void main(String[] args) {
+        try {
+            OutputStream os = new FileOutputStream("C:/Temp/test1.db");
+
+            byte a = 10;
+            byte b = 20;
+            int c = 300000000;
+
+            os.write(a);
+            os.write(b);
+            os.write(c);
+            os.write(c);
+
+            os.flush();
+            os.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
